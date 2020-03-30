8bc29fb43a093a0f3be1c9319700897a8168caf4
Christian Sparks <csparks1919@hotmail.com>
2020-03-30 16:11:18 -0500

    Fix interrupt code

--- interrupt_test/interrupt_test.ino
+++ interrupt_test/interrupt_test.ino
@@ -4,8 +4,11 @@ void setup() {
   TCCR1A = 0; //Set Timer/Counter Control Register A value to 0
 
   TCCR1B = 0; //Set Timer/Counter Control Register B value to 0
-  bitSet(TCCR1B, CS12);
-  bitSet(TIMSK1, TOIE1);
+  bitSet(TCCR1B, CS12); // Set up 256 prescaler
+  
+  OCR1A = 62500; // Timer interrupt compare register
+
+  bitSet(TIMSK1, TOIE1); // Timer/Counter 1 Interrupt Mask 
   pinMode(9, OUTPUT);
 }
 
